dec9f2dc09b6f49d6e09a65abc35d205f059712f
xiaotushaoxia <644653701@qq.com>
2023-03-28 21:41:37 +0800

    1. add cas swap cad. 2. show benchmark result with map and rwmutexmap

--- syncmap.go
+++ syncmap.go
@@ -7,6 +7,34 @@ import "sync"
 // 这2个测试结果应该还是比较好的  所以写多的情况下(简单来说是写多的情况)应该用RWMutexMap而不是SyncMap
 
 // Map
+//
+//	20230328	/src/sync/map_bench_test.go   根据这测试Map意外的好  看来几乎没有情况要用rwmutexmap了
+//	                          name   copy on write map  rwmutex map          map
+//
+// 0                 LoadMostlyHits       6.463 ns/op  43.40 ns/op  6.134 ns/op
+// 1               LoadMostlyMisses       5.512 ns/op  53.78 ns/op  6.652 ns/op
+// 2            LoadOrStoreBalanced          19 ns/op   85.6 ns/op   28.0 ns/op
+// 3              LoadOrStoreUnique          47 ns/op   42.3 ns/op   21.2 ns/op
+// 4           LoadOrStoreCollision       2.991 ns/op  68.37 ns/op  3.124 ns/op
+// 5          LoadAndDeleteBalanced        34.5 ns/op  70.39 ns/op  6.087 ns/op
+// 6            LoadAndDeleteUnique        43.7 ns/op  76.12 ns/op  6.494 ns/op
+// 7         LoadAndDeleteCollision        82.0 ns/op  68.77 ns/op  1.901 ns/op
+// 8                          Range          29 ns/op     16 ns/op     85 ns/op
+// 9               AdversarialAlloc          86 ns/op  64.77 ns/op   95.9 ns/op
+// 10             AdversarialDelete        84.4 ns/op  82.19 ns/op  52.48 ns/op
+// 11               DeleteCollision        17.7 ns/op  62.97 ns/op  3.525 ns/op
+// 12                 SwapCollision        14.8 ns/op  98.14 ns/op   17.4 ns/op
+// 13                SwapMostlyHits          76 ns/op   54.9 ns/op  24.95 ns/op
+// 14              SwapMostlyMisses          35 ns/op   38.6 ns/op   69.7 ns/op  x
+// 15       CompareAndSwapCollision       3.642 ns/op  82.75 ns/op  7.159 ns/op
+// 16   CompareAndSwapNoExistingKey       5.608 ns/op  73.59 ns/op  6.180 ns/op
+// 17   CompareAndSwapValueNotEqual       3.391 ns/op  73.54 ns/op  3.793 ns/op
+// 18      CompareAndSwapMostlyHits       78247 ns/op  181.2 ns/op  28.31 ns/op
+// 19    CompareAndSwapMostlyMisses       14.22 ns/op  117.4 ns/op  13.08 ns/op
+// 20     CompareAndDeleteCollision       1.900 ns/op  62.05 ns/op  4.706 ns/op
+// 21    CompareAndDeleteMostlyHits      147829 ns/op  297.1 ns/op  36.65 ns/op
+// 22  CompareAndDeleteMostlyMisses       10.15 ns/op  90.41 ns/op  9.881 ns/op
+//
 // go文档写 sync.Map优化了这两种情况 1很少写很多读 2读写覆盖多个不同key
 // The Map type is optimized for two common use cases: (1) when the entry for a given
 // key is only ever written once but read many times, as in caches that only grow,
@@ -31,7 +59,17 @@ func (m *Map[K, V]) Store(key K, value V) {
 
 func (m *Map[K, V]) LoadOrStore(key K, value V) (actual V, loaded bool) {
 	a, loaded := m.mp.LoadOrStore(key, value)
-	actual = a.(V)
+	if a != nil {
+		actual = a.(V)
+	}
+	return
+}
+
+func (m *Map[K, V]) LoadAndDelete(key K) (value V, loaded bool) {
+	a, loaded := m.mp.LoadAndDelete(key)
+	if a != nil {
+		value = a.(V)
+	}
 	return
 }
 
@@ -45,9 +83,24 @@ func (m *Map[K, V]) Range(f func(key K, value V) (shouldContinue bool)) {
 	})
 }
 
+func (m *Map[K, V]) Swap(key K, value V) (previous V, loaded bool) {
+	p, loaded := m.mp.Swap(key, value)
+	if loaded {
+		return p.(V), loaded
+	}
+	return
+}
+
+func (m *Map[K, V]) CompareAndSwap(key K, old, newV V) (swapped bool) {
+	return m.mp.CompareAndSwap(key, old, newV)
+}
+
+func (m *Map[K, V]) CompareAndDelete(key K, old V) (deleted bool) {
+	return m.mp.CompareAndDelete(key, old)
+}
+
 // RWMutexMap is an implementation of mapInterface using a sync.RWMutex.
 // 来自go标准库里面的一个文件 src/sync/map_reference_test.go   用来读少写少的情况下使用这个还是不错的(比syncmap好)，不知道为啥没有加入标准库
-
 type RWMutexMap[K comparable, V any] struct {
 	mu    sync.RWMutex
 	dirty map[K]V
@@ -80,7 +133,7 @@ func (m *RWMutexMap[K, V]) LoadOrStore(key K, value V) (actual V, loaded bool) {
 		m.dirty[key] = value
 	}
 	m.mu.Unlock()
-	return actual, loaded
+	return
 }
 
 func (m *RWMutexMap[K, V]) LoadAndDelete(key K) (value V, loaded bool) {
@@ -88,12 +141,11 @@ func (m *RWMutexMap[K, V]) LoadAndDelete(key K) (value V, loaded bool) {
 	value, loaded = m.dirty[key]
 	if !loaded {
 		m.mu.Unlock()
-		var v V
-		return v, false
+		return
 	}
 	delete(m.dirty, key)
 	m.mu.Unlock()
-	return value, loaded
+	return
 }
 
 func (m *RWMutexMap[K, V]) Delete(key K) {
@@ -119,4 +171,50 @@ func (m *RWMutexMap[K, V]) Range(f func(key K, value V) (shouldContinue bool)) {
 			break
 		}
 	}
+}
+
+func (m *RWMutexMap[K, V]) Swap(key K, value V) (previous V, loaded bool) {
+	m.mu.Lock()
+	if m.dirty == nil {
+		m.dirty = make(map[K]V)
+	}
+
+	previous, loaded = m.dirty[key]
+	m.dirty[key] = value
+	m.mu.Unlock()
+	return
+}
+
+func (m *RWMutexMap[K, V]) CompareAndSwap(key K, old, newV V) (swapped bool) {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+	if m.dirty == nil {
+		return false
+	}
+
+	value, loaded := m.dirty[key]
+	if loaded && eq(value, old) {
+		m.dirty[key] = newV
+		return true
+	}
+	return false
+}
+
+func (m *RWMutexMap[K, V]) CompareAndDelete(key K, old V) (deleted bool) {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+	if m.dirty == nil {
+		return false
+	}
+
+	value, loaded := m.dirty[key]
+	if loaded && eq(value, old) {
+		delete(m.dirty, key)
+		return true
+	}
+	return false
+}
+
+func eq(a, b any) bool {
+	return a == b
 }
--- syncmap_test.go
+++ syncmap_test.go
@@ -13,4 +13,22 @@ func TestName(t *testing.T) {
 		fmt.Println(i, j)
 		return true
 	})
+
+	swapped := a.CompareAndSwap(1, 2, 2)
+	fmt.Println(swapped)
+	a.Range(func(i, j int) bool {
+		fmt.Println(i, j)
+		return true
+	})
+
+}
+
+func TestMap(t *testing.T) {
+	var m Map[int, string]
+
+	//actual, loaded := m.LoadOrStore(1, "10")
+	//fmt.Println(actual)
+	//fmt.Println(loaded)
+	//
+	fmt.Println(m.Swap(1, "200"))
 }
